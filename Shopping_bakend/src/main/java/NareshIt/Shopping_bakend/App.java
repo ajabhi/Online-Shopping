7d6084c6cdee751185401b4651b20f29915c5cf2
Abhishek Raj <abhiraj9673@gmail.com>
2019-02-18 01:30:13 +0530

    My project setup completed successfully

---
+++ Shopping_bakend/src/main/java/NareshIt/Shopping_bakend/App.java
@@ -0,0 +1,13 @@
+package NareshIt.Shopping_bakend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
